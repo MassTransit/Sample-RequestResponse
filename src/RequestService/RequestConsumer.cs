fec6cffe162169d8431b75e7c627d76c387e2508
Chris Patterson <chris@phatboyg.com>
2015-05-25 18:46:50 -0700

    Added log message for request consumer

--- src/RequestService/RequestConsumer.cs
+++ src/RequestService/RequestConsumer.cs
@@ -2,14 +2,19 @@
 {
     using System.Threading.Tasks;
     using MassTransit;
+    using MassTransit.Logging;
     using Sample.MessageTypes;
 
 
     public class RequestConsumer :
         IConsumer<ISimpleRequest>
     {
+        readonly ILog _log = Logger.Get<RequestConsumer>();
+
         public async Task Consume(ConsumeContext<ISimpleRequest> context)
         {
+            _log.InfoFormat("Returning name for {0}", context.Message.CustomerId);
+
             context.Respond(new SimpleResponse
             {
                 CusomerName = string.Format("Customer Number {0}", context.Message.CustomerId)
